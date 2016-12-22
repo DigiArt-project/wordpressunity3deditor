70a1096d55f76ed0846555c778be751ec986eab9
stathis.nikolaidis <stathis.nikolaidis@gmail.com>
2016-12-22 10:49:27 +0200

    Create Initial Data on plugin Activation

--- WordpressUnity3DEditor-functions.php
+++ WordpressUnity3DEditor-functions.php
@@ -54,6 +54,11 @@ include_once( plugin_dir_path( __FILE__ ) . 'includes/wpunity-core-functions.php
 //2.03 Create Asset Scene
 include_once( plugin_dir_path( __FILE__ ) . 'includes/wpunity-types-assets.php' );
 
+
+//3.01 Create Initial Asset Categories
+include_once( plugin_dir_path( __FILE__ ) . 'includes/wpunity-core-insertData.php' );
+register_activation_hook( __FILE__, 'wpunity_assets_taxcategory_fill' );
+
 /**
  * Game cpt
  */
--- includes/wpunity-types-assets.php
+++ includes/wpunity-types-assets.php
@@ -127,23 +127,8 @@ class Asset3DClass{
 
     }
 
-
-
     /**
      * 2.03
-     * Create Initial Asset Categories
-     *
-     */
-
-    function wpunity_assets_taxcategory_fill(){
-        wp_insert_term('Dynamic 3D models','asset3d_category','dynamic3dmodels','Dynamic 3D models are those that can be clicked or moved, e.g. artifacts.');
-        wp_insert_term('Points of Interest','asset3d_category','pois','Points of interest (POIs) are spots at the game where information pops up.');
-        wp_insert_term('Static 3D models','asset3d_category','static3dmodels','Static 3D models are those that can not be clicked and can not be moved (e.g. ground, wall, cave, house)');
-        wp_insert_term('Doors','asset3d_category','doors','Doors are 3D model where avatar pass through and thus going from one Scene to another Scene');
-    }
-
-    /**
-     * 2.04
      * Create Asset Scene
      *
      * Select To Which Scenes it belongs to (as custom taxonomy)
