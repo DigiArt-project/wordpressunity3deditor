f5324b11478483f9eeebd7a869de79f8fcbd48b7
jimver <jimver04@gmail.com>
2017-02-17 15:30:38 +0200

    I Added also the assemble/compiled game

---
+++ games_assemble/dune/starter_artificial.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Unity\Editor\Unity.exe" -quit -batchmode -logFile C:\\xampp\\htdocs\\digiart-project_Jan17\\wp-content\\plugins\\wordpressunity3deditor\\games_assemble\\dune\stdout.log -projectPath C:\\xampp\\htdocs\\digiart-project_Jan17\\wp-content\\plugins\\wordpressunity3deditor\\games_assemble\\dune -buildWindowsPlayer "builds\mygame.exe"
