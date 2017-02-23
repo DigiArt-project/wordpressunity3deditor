049bb9e290bcab64c6c99d9e02f2012862ba22b3
jimver <jimver04@gmail.com>
2017-02-23 12:58:13 +0200

    - Added General_Scripts in the unity_game_libraries (windows and linux). Only the all_menu_Script.cs has to be overwritten in the end.

---
+++ unity_game_libraries/linux/Assets/General_Scripts/doorIntent_Script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorIntent_Script : MonoBehaviour {
+
+	public string doorID = "";
+
+	void Start () {
+		DontDestroyOnLoad (this);
+	}
+	
+}
---
+++ unity_game_libraries/windows/Assets/General_Scripts/door_Script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class door_Script : MonoBehaviour {
+
+	public string sceneArrival;
+	public string doorArrival;
+
+	void OnTriggerEnter( Collider col) {
+
+		if(col.gameObject.name == "Player"){
+
+			// Get The door to arrive at 
+			GameObject doorIntent = GameObject.Find ("doorIntent");
+			doorIntent_Script dis = doorIntent.GetComponent<doorIntent_Script> (); 
+			dis.doorID = doorArrival;
+
+			// Load the Scene to arrive at 
+			SceneManager.LoadScene(sceneArrival);
+
+		}
+	}
+
+}
+
