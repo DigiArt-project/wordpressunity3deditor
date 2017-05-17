cc46fd44f81a9631c34df487238e2048b9e2d3df
Tasos Papazoglou - Chalikias <tpapazoglou@iti.gr>
2017-05-17 08:51:27 +0300

    Updated gitignore file. Started implementing Material Design Components. Untracked .log files

--- WordpressUnity3DEditor-functions.php
+++ WordpressUnity3DEditor-functions.php
@@ -47,27 +47,28 @@ wp_register_style( 'wpunity_backend', plugin_dir_url( __FILE__ ) . 'css/wpunity_
 wp_register_style( 'wpunity_vr_editor', plugin_dir_url( __FILE__ ) . 'css/vr_editor_style.css' );
 wp_register_style( 'wpunity_vr_editor_filebrowser', plugin_dir_url( __FILE__ ) . 'css/vr_editor_fileBrowserStyle.css' );
 
-
+wp_register_style( 'wpunity_material_stylesheet',  plugin_dir_url( __FILE__ ) . '/node_modules/material-components-web/dist/material-components-web.css' );
+wp_enqueue_style('wpunity_material_stylesheet');
 
 wp_register_script( 'wpunity_compile_request', plugin_dir_url( __FILE__ ) . 'js_libs/assemble_compile_commands/request_game_compile.js',
-    null, null, false);
+	null, null, false);
 
 wp_register_script( 'wpunity_assemble_request', plugin_dir_url( __FILE__ ) . 'js_libs/assemble_compile_commands/request_game_assemble.js',
-    null, null, false);
+	null, null, false);
 
 
 wp_register_script( 'wpunity_content_interlinking_request', plugin_dir_url( __FILE__ ) . 'js_libs/content_interlinking_commands/content_interlinking.js',
-    null, null, false);
+	null, null, false);
 
 wp_register_script( 'wpunity_segmentation_request', plugin_dir_url( __FILE__ ) . 'js_libs/semantics_commands/segmentation.js',
-    null, null, false);
+	null, null, false);
 
 wp_register_script( 'wpunity_classification_request', plugin_dir_url( __FILE__ ) . 'js_libs/semantics_commands/classification.js',
-    null, null, false);
+	null, null, false);
 
 
 wp_register_script( 'wpunity_fetch_asset_scenes_request', plugin_dir_url( __FILE__ ) . 'js_libs/scriptFileBrowserToolbarWPway.js',
-    null, null, false);
+	null, null, false);
 
 //=========================== THREE js scripts ============================================
 //wp_register_script( 'wpunity_load_threejs', plugin_dir_url( __FILE__ ) . 'js_libs/threejs79/three.js',
@@ -244,12 +245,12 @@ register_activation_hook(__FILE__,'wpunity_create_openGamePage');
  * @return mixed
  */
 function my_myme_types($mime_types){
-    $mime_types['json'] = 'text/json';
-    $mime_types['obj'] = 'text/plain';
-    $mime_types['mp4'] = 'video/mp4';
-    $mime_types['mtl'] = 'text/plain';
-    $mime_types['mat'] = 'text/plain';
-    return $mime_types;
+	$mime_types['json'] = 'text/json';
+	$mime_types['obj'] = 'text/plain';
+	$mime_types['mp4'] = 'video/mp4';
+	$mime_types['mtl'] = 'text/plain';
+	$mime_types['mat'] = 'text/plain';
+	return $mime_types;
 }
 
 add_filter('upload_mimes', 'my_myme_types', 1, 1);
@@ -257,14 +258,24 @@ add_filter('upload_mimes', 'my_myme_types', 1, 1);
 
 
 //Scripts about Upload button in Metaboxes
-add_action('plugins_loaded', function(){
-    if($GLOBALS['pagenow']=='post.php'){
-        add_action('admin_print_scripts', 'my_admin_scripts');
-        add_action('admin_print_styles',  'my_admin_styles');
-    }
+add_action('plugins_loaded', function() {
+	if($GLOBALS['pagenow']=='post.php') {
+		add_action('admin_print_scripts', 'my_admin_scripts');
+		add_action('admin_print_styles',  'my_admin_styles');
+	}
 });
 
-function my_admin_scripts() { wp_enqueue_script('jquery');    wp_enqueue_script('media-upload');   wp_enqueue_script('thickbox'); }   //  //wp_register_script('my-upload', WP_PLUGIN_URL.'/my-script.js', array('jquery','media-upload','thickbox'));  wp_enqueue_script('my-upload');
-function my_admin_styles()  { wp_enqueue_style('thickbox'); }
+function my_admin_scripts() {
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('media-upload');
+	wp_enqueue_script('thickbox');
+}
+
+	//wp_register_script('my-upload', WP_PLUGIN_URL.'/my-script.js', array('jquery','media-upload','thickbox'));
+	//  wp_enqueue_script('my-upload');
+function my_admin_styles()  {
+	wp_enqueue_style('thickbox');
+
+}
 
 ?>
