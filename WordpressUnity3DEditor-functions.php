47e0de2c8f4355980e87497eb7621b0e3c7977dc
Tasos Papazoglou - Chalikias <tpapazoglou@iti.gr>
2017-05-17 11:46:05 +0300

    Started working on First screen

--- WordpressUnity3DEditor-functions.php
+++ WordpressUnity3DEditor-functions.php
@@ -47,8 +47,15 @@ wp_register_style( 'wpunity_backend', plugin_dir_url( __FILE__ ) . 'css/wpunity_
 wp_register_style( 'wpunity_vr_editor', plugin_dir_url( __FILE__ ) . 'css/vr_editor_style.css' );
 wp_register_style( 'wpunity_vr_editor_filebrowser', plugin_dir_url( __FILE__ ) . 'css/vr_editor_fileBrowserStyle.css' );
 
-wp_register_style( 'wpunity_material_stylesheet',  plugin_dir_url( __FILE__ ) . '/node_modules/material-components-web/dist/material-components-web.css' );
+// Material Design Components
+wp_register_style( 'wpunity_material_stylesheet',  plugin_dir_url( __FILE__ ) . 'node_modules/material-components-web/dist/material-components-web.css' );
 wp_enqueue_style('wpunity_material_stylesheet');
+wp_register_script( 'wpunity_material_scripts', plugin_dir_url( __FILE__ ) . 'node_modules/material-components-web/dist/material-components-web.js');
+wp_enqueue_script('wpunity_material_scripts');
+
+// Frontend CSS
+wp_register_style( 'wpunity_frontend_stylesheet',  plugin_dir_url( __FILE__ ) . 'css/front.css' );
+wp_enqueue_style('wpunity_frontend_stylesheet');
 
 wp_register_script( 'wpunity_compile_request', plugin_dir_url( __FILE__ ) . 'js_libs/assemble_compile_commands/request_game_compile.js',
 	null, null, false);
--- includes/templates/open-wpunity_game.php
+++ includes/templates/open-wpunity_game.php
@@ -1,12 +1,73 @@
 <?php
 
-get_header();
+
+get_header(); ?>
 
 
+    <h1 class="mdc-typography--display4 mdc-theme--text-primary-on-light mdc-theme--text">Game Authoring Tool</h1>
 
-echo "Here comes our code";
+    <div class="mdc-layout-grid">
+
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+
+            <h2 class="mdc-typography--display3 mdc-theme--text-primary-on-light mdc-theme--text">Library</h2>
+
+            <ul class="mdc-list mdc-list--two-line mdc-list--avatar-list">
+                <li class="mdc-list-item">
+                    <a href="#" class="mdc-list-item mdc-ripple-upgraded mdc-ripple-upgraded--foreground-deactivation" data-mdc-auto-init="MDCRipple">
+                        <i class="material-icons mdc-list-item__start-detail" aria-hidden="true" title="Energy">
+                            blur_on
+                        </i>
+                        <span class="mdc-list-item__text mdc-typography--title">
+                            Put title from loop
+                            <span class="mdc-list-item__text__secondary mdc-typography--subheading2">Jan 10, 2014</span>
+                        </span>
+                    </a>
+                    <a href="#" class="mdc-list-item__end-detail material-icons" aria-label="View more information" title="Delete game" onclick="event.preventDefault();">
+                        delete
+                    </a>
+                </li>
+                <li class="mdc-list-item">
+                    <a href="#" class="mdc-list-item mdc-ripple-upgraded mdc-ripple-upgraded--foreground-deactivation" data-mdc-auto-init="MDCRipple">
+                        <i class="material-icons mdc-list-item__start-detail" aria-hidden="true" title="Energy">
+                            blur_on
+                        </i>
+                        <span class="mdc-list-item__text mdc-typography--title">
+                            Put title from loop
+                            <span class="mdc-list-item__text__secondary mdc-typography--subheading2">Jan 10, 2014</span>
+                        </span>
+                    </a>
+                    <a href="#" class="mdc-list-item__end-detail material-icons" aria-label="View more information" title="Delete game" onclick="event.preventDefault();">
+                        delete
+                    </a>
+                </li>
+                <li class="mdc-list-item">
+                    <a href="#" class="mdc-list-item mdc-ripple-upgraded mdc-ripple-upgraded--foreground-deactivation" data-mdc-auto-init="MDCRipple">
+                        <i class="material-icons mdc-list-item__start-detail" aria-hidden="true" title="Archaeology">
+                            account_balance
+                        </i>
+                        <span class="mdc-list-item__text mdc-typography--title">
+                            Put title from loop
+                            <span class="mdc-list-item__text__secondary mdc-typography--subheading2">Jan 10, 2014</span>
+                        </span>
+                    </a>
+                    <a href="#" class="mdc-list-item__end-detail material-icons" aria-label="View more information" title="Delete game" onclick="event.preventDefault();">
+                        delete
+                    </a>
+                </li>
+
+            </ul>
+        </div>
 
 
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+            <h2 class="mdc-typography--display3 mdc-theme--text-primary-on-light mdc-theme--text">New game</h2>
 
-get_footer();
+        </div>
+
+
+        <script type="text/javascript">
+            window.mdc.autoInit();
+        </script>
+<?php get_footer();
 ?>
