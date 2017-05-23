0751920e0bb69b943184edc850117aa422480e38
Tasos Papazoglou - Chalikias <tpapazoglou@iti.gr>
2017-05-23 10:59:46 +0300

    Start shaping edit scene view

--- includes/templates/edit-wpunity_scene.php
+++ includes/templates/edit-wpunity_scene.php
@@ -2,35 +2,33 @@
 
 get_header(); ?>
 
+    <div class="EditPageHeader">
 
+        <h1 class="mdc-typography--display1 mdc-theme--text-primary-on-light"><?php echo $game_post->post_title; ?></h1>
 
-	<div class="EditPageHeader">
+        <a class="mdc-button mdc-button mdc-button--raised mdc-button--primary" data-mdc-auto-init="MDCRipple">
+            COMPILE GAME
+        </a>
+    </div>
 
-		<h1 class="mdc-typography--display1 mdc-theme--text-primary-on-light"><?php echo $game_post->post_title; ?></h1>
-
-		<a class="mdc-button mdc-button mdc-button--raised mdc-button--primary" data-mdc-auto-init="MDCRipple">
-			COMPILE GAME
-		</a>
-	</div>
-
-	<span class="mdc-typography--caption">
+    <span class="mdc-typography--caption">
         <i class="material-icons mdc-theme--text-icon-on-background AlignIconToBottom" title="Add category title & icon"><?php echo $game_type_obj->icon; ?> </i>&nbsp;<?php echo $game_type_obj->string; ?></span>
 
-	<hr class="mdc-list-divider">
+    <hr class="mdc-list-divider">
 
-	<ul class="EditPageBreadcrumb">
-		<li><a class="mdc-typography--caption mdc-theme--accent" href="#">Home</a></li>
-		<li><i class="material-icons EditPageBreadcrumbArr mdc-theme--text-hint-on-background">arrow_drop_up</i></li>
-		<li class="mdc-typography--caption"><span class="EditPageBreadcrumbSelected">Game Editor</span></li>
-		<li><i class="material-icons EditPageBreadcrumbArr mdc-theme--text-hint-on-background">arrow_drop_up</i></li>
-		<li class="mdc-typography--caption"><span class="EditPageBreadcrumbSelected">Scene Editor</span></li>
-	</ul>
+    <ul class="EditPageBreadcrumb">
+        <li><a class="mdc-typography--caption mdc-theme--accent" href="#">Home</a></li>
+        <li><i class="material-icons EditPageBreadcrumbArr mdc-theme--text-hint-on-background">arrow_drop_up</i></li>
+        <li class="mdc-typography--caption"><span class="EditPageBreadcrumbSelected">Game Editor</span></li>
+        <li><i class="material-icons EditPageBreadcrumbArr mdc-theme--text-hint-on-background">arrow_drop_up</i></li>
+        <li class="mdc-typography--caption"><span class="EditPageBreadcrumbSelected">Scene Editor</span></li>
+    </ul>
 
-	<h2 class="mdc-typography--headline mdc-theme--text-primary-on-light">Scenes</h2>
+    <h2 class="mdc-typography--headline mdc-theme--text-primary-on-light">Scenes</h2>
 
 
-	<script type="text/javascript">
+    <script type="text/javascript">
         window.mdc.autoInit();
-	</script>
+    </script>
 
 <?php get_footer(); ?>
