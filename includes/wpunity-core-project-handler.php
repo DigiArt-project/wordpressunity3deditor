532a58855e1ce926757362c429fc65aff383c386
stathis.nikolaidis <stathis.nikolaidis@gmail.com>
2017-06-20 16:11:52 +0300

    Fix Upload Folder for Assets & Scenes

--- includes/wpunity-core-project-handler.php
+++ includes/wpunity-core-project-handler.php
@@ -27,12 +27,12 @@ function wpunity_upload_dir_forAssets( $args ) {
 
         if ( get_post_type( $id ) == 'wpunity_asset3d' ) {
 
-            global $post;
-            $gameterms = get_the_terms( $post->ID , 'wpunity_asset3d_pgame' );
-            $projectSlug = $gameterms[0]->slug; // wp_get_object_terms( $post->ID, 'wpunity_asset3d_pgame')[0]->slug; //
-
-            // Set the new path depends on current post_type
-            $newdir =  '/' . $projectSlug . '/Models';
+//            global $post;
+//            $gameterms = get_the_terms( $post->ID , 'wpunity_asset3d_pgame' );
+//            $projectSlug = $gameterms[0]->slug; // wp_get_object_terms( $post->ID, 'wpunity_asset3d_pgame')[0]->slug; //
+            $type_IDs = wp_get_object_terms( $id, 'wpunity_asset3d_pgame', array('fields' => 'ids') );
+            $gameProject = get_term_by('id', $type_IDs[0], 'wpunity_asset3d_pgame');
+            $newdir =  '/' . $gameProject->slug . '/Models';
 
             $args['path']    = str_replace( $args['subdir'], '', $args['path'] ); //remove default subdir
             $args['url']     = str_replace( $args['subdir'], '', $args['url'] );
