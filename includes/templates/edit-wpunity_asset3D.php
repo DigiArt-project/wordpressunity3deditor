e051c5413c395fc9a3cddbd2f97efc9507b18510
Tasos Papazoglou - Chalikias <tpapazoglou@iti.gr>
2017-07-12 15:43:36 +0300

    3D asset editor: Producer chart: Changed the default values.

--- includes/templates/edit-wpunity_asset3D.php
+++ includes/templates/edit-wpunity_asset3D.php
@@ -523,22 +523,22 @@ get_header(); ?>
 
                     <span>2</span>
                     <span>2</span>
-                    <span>2</span>
-                    <span>3</span>
-                    <span>3</span>
-                    <span>3</span>
-                    <span>3</span>
-                    <span>3</span>
-                    <span>4</span>
-                    <span>4</span>
-                    <span>4</span>
+                    <span>5</span>
+                    <span>5</span>
+                    <span>5</span>
+                    <span>5</span>
+                    <span>5</span>
+                    <span>5</span>
+                    <span>5</span>
+                    <span>5</span>
+                    <span>5</span>
 
-                    <span>4</span>
                     <span>5</span>
                     <span>5</span>
                     <span>5</span>
-                    <span>6</span>
-                    <span>6</span>
+                    <span>5</span>
+                    <span>0</span>
+                    <span>0</span>
                 </div>
 
                 <input type="hidden" id="producerPowerProductionVal" value="" disabled>
