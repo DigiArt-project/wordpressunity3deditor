f40d4b98a602d737990575944b958ea81eb03f73
jimver <jimver04@gmail.com>
2017-09-08 16:30:47 +0300

    - Fix pause and simulation speed widgets - New paketo 3d

--- includes/templates/edit-wpunity_asset3D.php
+++ includes/templates/edit-wpunity_asset3D.php
@@ -1023,7 +1023,9 @@ get_header(); ?>
 
 		<?php if($game_type_obj->string == 'Energy') {
 			echo "<p>Help: Packet of 3D models for game type: " . $game_type_obj->string . "</p>" ;
-			echo "<a href='https://www.dropbox.com/s/0ed7f9c6v4qnsnl/paketo_3d_v3.zip?dl=0'>Energy Lab 3D models</a>";
+			echo "<a href='".plugins_url( '../assets/paketo_3d_v3.zip', dirname(__FILE__)  )."'>Energy Lab 3D models</a>";
+
+
 		}
 		?>
     </form>
