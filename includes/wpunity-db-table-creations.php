0f96bdc29cd300a551a4d30a5213b945956e2653
Tasos Papazoglou - Chalikias <tpapazoglou@iti.gr>
2017-10-19 14:46:09 +0300

    Fixed an sql query error that prevented the new table (game_versions_table) to be created on plugin activation

--- includes/wpunity-db-table-creations.php
+++ includes/wpunity-db-table-creations.php
@@ -7,27 +7,27 @@ $wpunity_db_version = '1.0';
  *     Create the table of games versions
  */
 function wpunity_db_create_games_versions_table () {
-    global $wpdb;
-    global $wpunity_db_version;
+	global $wpdb;
+	global $wpunity_db_version;
 
-    echo "generating table";
+	echo "generating table";
 
-    $table_name = $wpdb->prefix . "_games_versions";
+	$table_name = $wpdb->prefix . "_games_versions";
 
-    $charset_collate = $wpdb->get_charset_collate();
+	$charset_collate = $wpdb->get_charset_collate();
 
-    $sql = "CREATE TABLE $table_name (
+	$sql = "CREATE TABLE $table_name (
   id mediumint(9) NOT NULL AUTO_INCREMENT,
   game_project_id int NOT NULL,
   version_number int NOT NULL,
-  date_generated datetime DEFAULT (getdate()) NOT NULL,
+  date_generated DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY  (id)
 ) $charset_collate;";
 
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-    dbDelta( $sql );
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
 
-    add_option( 'wpunity_db_version', $wpunity_db_version );
+	add_option( 'wpunity_db_version', $wpunity_db_version );
 }
 
 /**
@@ -38,39 +38,37 @@ function wpunity_db_create_games_versions_table () {
  * @param $new_version_number
  */
 function wpunity_append_version_game($game_project_id, $new_version_number) {
-    global $wpdb;
-    $table_name = $wpdb->prefix . "_games_versions";
+	global $wpdb;
+	$table_name = $wpdb->prefix . "_games_versions";
 
-    return $wpdb->insert(
-        $table_name,
-        array(
-            'game_project_id' => $game_project_id,
-            'version_number' => $new_version_number
-        )
-    );
+	return $wpdb->insert(
+		$table_name,
+		array(
+			'game_project_id' => $game_project_id,
+			'version_number' => $new_version_number
+		)
+	);
 }
 
 
 function wpunity_get_all_versions_of_game($game_project_id){
-    global $wpdb;
-    $table_name = $wpdb->prefix . "_games_versions";
+	global $wpdb;
+	$table_name = $wpdb->prefix . "_games_versions";
 
-    $arrayObjectOfVersions = $wpdb->get_results( 'SELECT * FROM $table_name WHERE game_project_id = $game_project_id', OBJECT );
+	$arrayObjectOfVersions = $wpdb->get_results( 'SELECT * FROM $table_name WHERE game_project_id = $game_project_id', OBJECT );
 
-    return $arrayObjectOfVersions;
+	return $arrayObjectOfVersions;
 }
 
 
 function wpunity_get_last_version_of_game($game_project_id){
 
-    global $wpdb;
-    $table_name = $wpdb->prefix . "_games_versions";
+	global $wpdb;
+	$table_name = $wpdb->prefix . "_games_versions";
 
-    $lastverion = -1;
+	$lastverion = -1;
 
-    $lastverion = $wpdb->get_results( 'SELECT max(version_number) FROM $table_name WHERE game_project_id = $game_project_id', OBJECT );
+	$lastverion = $wpdb->get_results( 'SELECT max(version_number) FROM $table_name WHERE game_project_id = $game_project_id', OBJECT );
 
-    return $lastverion;
+	return $lastverion;
 }
-
-
