b507d4ad6a4b02792f41f17113de0b26e7d6d905
jimver <jimver04@gmail.com>
2017-10-27 06:58:28 +0300

    - 3D Editor light fix minor

--- js_libs/vr_editor_environmentals.js
+++ js_libs/vr_editor_environmentals.js
@@ -424,7 +424,7 @@ class vr_editor_environmentals {
         this.lightOrbit.name = "mylightOrbit";
         this.scene.add(this.lightOrbit);
 
-        this.lightAvatar = new THREE.PointLight( 0xffffff, 0.01, 1000, 2 ); //THREE.DirectionalLight( 0xffffff, 1 );
+        this.lightAvatar = new THREE.PointLight( 0xffffff, 0.1, 1000, 2 ); //THREE.DirectionalLight( 0xffffff, 1 );
 
         this.lightAvatar.name = "mylightAvatar";
 
