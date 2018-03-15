50298553f2a57e946a01c92fe5b2560b83d049ea
Stathis Nikolaidis <stathis.nikolaidis@gmail.com>
2018-03-15 05:11:54 +0200

    Add register link to main theme page

--- includes/templates/open-wpunity_game.php
+++ includes/templates/open-wpunity_game.php
@@ -81,6 +81,7 @@ $user_id = get_current_user_id();
     <div class="DisplayBlock CenterContents">
         <i style="font-size: 64px; padding-top: 80px;" class="material-icons mdc-theme--text-icon-on-background">account_circle</i>
         <p class="mdc-typography--title"> Please <a class="mdc-theme--secondary" href="<?php echo wp_login_url( get_permalink() ); ?>">login</a> to use platform</p>
+        <p class="mdc-typography--title"> Or <a class="mdc-theme--secondary" href="<?php echo wp_registration_url( get_permalink() ); ?>">register</a> if you don't have an account</p>
     </div>
 
     <hr class="WhiteSpaceSeparator">
