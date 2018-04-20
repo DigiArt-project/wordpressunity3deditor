779bf23e7a2ba353ba0c7a1a4a08dfdffeb0f52e
stathis.nikolaidis <stathis.nikolaidis@gmail.com>
2018-04-20 15:23:18 +0300

    Add Molecule Image Thumb to getAllMolecules function

--- includes/wpunity-core-functions.php
+++ includes/wpunity-core-functions.php
@@ -1041,8 +1041,9 @@ function wpunity_get_all_molecules_of_game($project_id){
 			$custom_query->the_post();
 			$molecule_id = get_the_ID();
 			$molecule_title = get_the_title();
+			$the_featured_image_url = get_the_post_thumbnail_url($molecule_id);
 
-			$moleculesIds[] = ['moleculeID'=>$molecule_id, 'moleculeName'=>$molecule_title ];
+			$moleculesIds[] = ['moleculeID'=>$molecule_id, 'moleculeName'=>$molecule_title, 'moleculeImage'=>$the_featured_image_url ];
 		}
 	}
 
