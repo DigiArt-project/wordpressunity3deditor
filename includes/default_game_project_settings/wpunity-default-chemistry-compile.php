9d8921d4c84ced525987fbbec0355339a488c6e2
stathis.nikolaidis <stathis.nikolaidis@gmail.com>
2018-05-09 11:55:35 +0300

    Issue #299 -> Empty Chemistry Wander Around Scene YAML & Replace Function created

--- includes/default_game_project_settings/wpunity-default-chemistry-compile.php
+++ includes/default_game_project_settings/wpunity-default-chemistry-compile.php
@@ -13,7 +13,36 @@ function wpunity_addAssets_chemistry_lab_unity($scene_id){}
 //==========================================================================================================================================
 
 
-function wpunity_replace_chemistry_lab_unity($term_meta_educational_energy,$scene_id){}
+function wpunity_replace_chemistry_lab_unity($term_meta_wander_around_chem,$scene_id){
+
+    $scene_json = get_post_meta($scene_id,'wpunity_scene_json_input',true);
+
+    $jsonScene = htmlspecialchars_decode ( $scene_json );
+    $sceneJsonARR = json_decode($jsonScene, TRUE);
+
+    foreach ($sceneJsonARR['objects'] as $key => $value ) {
+        if ($key == 'avatarYawObject') {
+            $x_pos = - $value['position'][0]; // x is in the opposite site in unity
+            $y_pos = $value['position'][1];
+            $z_pos = $value['position'][2];
+            $x_player_rot = $value['quaternion_player'][0];
+            $y_player_rot = $value['quaternion_player'][1];
+            $z_player_rot = $value['quaternion_player'][2];
+            $w_player_rot = $value['quaternion_player'][3];
+        }
+    }
+    $file_content_return = str_replace("___[player_position_x]___",$x_pos,$term_meta_wander_around_chem);
+    $file_content_return = str_replace("___[player_position_y]___",$y_pos,$file_content_return);
+    $file_content_return = str_replace("___[player_position_z]___",$z_pos,$file_content_return);
+
+    $file_content_return = str_replace("___[player_rotation_x]___",$x_player_rot,$file_content_return);
+    $file_content_return = str_replace("___[player_rotation_y]___",$y_player_rot,$file_content_return);
+    $file_content_return = str_replace("___[player_rotation_z]___",$z_player_rot,$file_content_return);
+    $file_content_return = str_replace("___[player_rotation_w]___",$w_player_rot,$file_content_return);
+
+    return $file_content_return;
+
+}
 
 function wpunity_replace_room_unity($room_yaml,$room_fid,$room_mesh_fid,$room_mesh_collider_fid,$room_obj_guid,$room_position_x,$room_position_y,$room_position_z,$room_rotation_x,$room_rotation_y,$room_rotation_z,$room_rotation_w,$room_scale_x,$room_scale_y,$room_scale_z,$room_title){
     $file_content_return = str_replace("___[room_fid]___",$room_fid,$room_yaml);
