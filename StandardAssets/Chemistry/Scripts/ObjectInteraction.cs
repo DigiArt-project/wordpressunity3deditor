c047b85c0b900b514db24582662292eca7a1c847
stathis.nikolaidis <stathis.nikolaidis@gmail.com>
2018-05-14 12:37:58 +0300

    StandardAssets folder for Chemistry projects

---
+++ StandardAssets/Chemistry/Scripts/ObjectInteraction.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using goedle_sdk;
+
+public class ObjectInteraction : MonoBehaviour
+{
+
+    private bool inTrigger = false;
+    private Renderer rend;
+
+    [SerializeField] private Canvas interactionCanvas;
+    [SerializeField] private Canvas crosshair;
+
+    [SerializeField] private Material defaultMat; //used to store the default and the outiline materials.
+    [SerializeField] private Material outlinedMat;
+
+    private enum Interactables {naming, construction,exit}
+    [SerializeField] private Interactables interactable;
+
+    public SceneLoader sceneLoader;
+    public CursorLock cursor;
+    public AudioClip clip;
+    public string sceneToLoad;
+    public ScoreManagerDisplay scoreManager;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        interactionCanvas.enabled = false;
+        //apply the default material
+        rend = GetComponent<Renderer>();
+        rend.material = defaultMat;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inTrigger == true)
+        {
+            InteractWithObject();
+            if(interactable != Interactables.exit)
+            {
+                GameManager.instance.lastPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position;
+                GameManager.instance.lastRotation = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().rotation;
+            }
+        }
+    }
+
+
+    private void InteractWithObject()
+    {
+        if (interactable == Interactables.naming)
+        {
+            GameManager.currentLevel = GameManager.Levels.moleculeNaming;
+            cursor.UnLockCursor();
+            GoedleAnalytics.instance.track("choose.quiz",sceneToLoad);
+            //MoleculeManager.instance.buildStrategyNamingQueue(MoleculeManager.instance._naming_strategy);
+            sceneLoader.LoadScene(sceneToLoad);
+        }
+        else if (interactable == Interactables.construction)
+        {
+            GameManager.currentLevel = GameManager.Levels.moleculeConstruction;
+            cursor.UnLockCursor();
+            GoedleAnalytics.instance.track("choose.quiz", sceneToLoad);
+            //MoleculeManager.instance.buildStrategyConstructionQueue(MoleculeManager.instance._construction_strategy);
+            sceneLoader.LoadScene(sceneToLoad);
+        }
+        else if (interactable == Interactables.exit)
+        {
+            interactionCanvas.enabled = false;
+            cursor.UnLockCursor();
+            scoreManager.DisplayScore();
+        }
+        SoundManager.instance.PlaySingle(clip);
+    }
+
+    #region trigger with player
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GoedleAnalytics.instance.track("play");
+
+            rend.material = outlinedMat;
+            interactionCanvas.enabled = true;
+            inTrigger = true;
+            crosshair.GetComponent<Crosshair>().DisableCrosshair();
+
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        rend.material = defaultMat;
+        interactionCanvas.enabled = false;
+        inTrigger = false;
+        crosshair.GetComponent<Crosshair>().EnableCrosshair();
+    }
+
+    #endregion
+
+}
---
+++ StandardAssets/Chemistry/Scripts/UserIdentification.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;                
+using System.Linq;
+using goedle_sdk;
+using goedle_sdk.detail;
+
+public class UserIdentification : MonoBehaviour {
+
+    public InputField[] inputFields;
+    public GameObject textLog;
+    public SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        textLog.SetActive(false);
+    }
+
+
+    public void StartSimulation(string sceneName)
+    {
+		string user_id_raw = GameManager.instance.playerName + GameManager.instance.playerClass + GameManager.instance.playerSchoolName;
+        // Creating a hashed user id, md5 hash of a string and then using a guid
+		string hashed_user_id = GoedleUtils.userHash(user_id_raw);
+        goedle_sdk.GoedleAnalytics.instance.setUserId(hashed_user_id);
+        sceneLoader.LoadScene(sceneName);
+    }
+
+}
