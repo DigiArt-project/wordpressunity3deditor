df4e67d0335088abaad8ca6015a421490bbf5573
Tasos Papazoglou - Chalikias <tpapazoglou@iti.gr>
2018-06-25 13:32:54 +0300

    Molecule Strategies are set only during compile

--- includes/templates/edit-wpunity_sceneExam.php
+++ includes/templates/edit-wpunity_sceneExam.php
@@ -80,8 +80,6 @@ if(isset($_POST['submitted']) && isset($_POST['post_nonce_field']) && wp_verify_
 	$savedStrategies = $_POST['json-strategies-input'];
 	update_post_meta($scene_id, 'wpunity_exam_strategy', $savedStrategies);
 
-	wpunity_addStrategy_APIcall($project_id);
-
     wp_redirect($goBackTo_MainLab_link);
 	exit;
 }
