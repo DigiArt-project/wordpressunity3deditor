a4de3c0d5aba11294bc764e628329aa99d758473
Stathis Nikolaidis <stathis.nikolaidis@gmail.com>
2018-07-02 19:48:36 +0300

    test commit

--- includes/default_game_project_settings/wpunity-default-chemistry-compile.php
+++ includes/default_game_project_settings/wpunity-default-chemistry-compile.php
@@ -37,9 +37,9 @@ function wpunity_create_chemistry_mainmenu_unity($scene_post,$scene_type_ID,$sce
     wpunity_add_in_HandyBuilder_cs($handybuilder_file, null, $file1_path_CS);
 
     //Add Static Pages to cs & BuildSettings (Main Menu must be first)
-//    wpunity_append_scenes_in_EditorBuildSettings_dot_asset($fileEditorBuildSettings,'Assets/scenes/S_Reward.unity');//Update the EditorBuildSettings.asset by adding new Scene
-//    $file_path_rewCS = 'Assets/scenes/' . 'S_Reward.unity';
-//    wpunity_add_in_HandyBuilder_cs($handybuilder_file, null, $file_path_rewCS);
+    //wpunity_append_scenes_in_EditorBuildSettings_dot_asset($fileEditorBuildSettings,'Assets/scenes/S_Reward.unity');//Update the EditorBuildSettings.asset by adding new Scene
+    //$file_path_rewCS = 'Assets/scenes/' . 'S_Reward.unity';
+    //wpunity_add_in_HandyBuilder_cs($handybuilder_file, null, $file_path_rewCS);
 
     //wpunity_append_scenes_in_EditorBuildSettings_dot_asset($fileEditorBuildSettings,'Assets/scenes/S_SceneSelector.unity');//Update the EditorBuildSettings.asset by adding new Scene
     //$file_path_selCS = 'Assets/scenes/' . 'S_SceneSelector.unity';
