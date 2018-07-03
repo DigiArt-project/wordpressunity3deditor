99ba617de894336268496c654d4ab7a6cd321a52
stathis.nikolaidis <stathis.nikolaidis@gmail.com>
2018-07-03 16:41:55 +0300

    Hide Producer/Consumer taxonomies

--- includes/default_game_project_settings/wpunity-default-energy-settings.php
+++ includes/default_game_project_settings/wpunity-default-energy-settings.php
@@ -32,29 +32,29 @@ function wpunity_assets_taxcategory_energy_fill(){
   update_term_meta($inserted_term7->term_id, 'wpunity_yamlmeta_assetcat_pat', wpunity_default_value_decoration_energy_get(), true);
   update_term_meta($inserted_term7->term_id, 'wpunity_assetcat_gamecat', 2 , true);
 
-  wp_insert_term(
-      'Consumer', // the term
-      'wpunity_asset3d_cat', // the taxonomy
-      array(
-          'description'=> 'A Consumer is a game object that consumes energy (e.g. a building).',
-          'slug' => 'consumer',
-      )
-  );
-  $inserted_term8 = get_term_by('slug', 'consumer', 'wpunity_asset3d_cat');
-  update_term_meta($inserted_term8->term_id, 'wpunity_yamlmeta_assetcat_pat', wpunity_default_value_consumer_get(), true);
-  update_term_meta($inserted_term8->term_id, 'wpunity_assetcat_gamecat', 2 , true);
+//  wp_insert_term(
+//      'Consumer', // the term
+//      'wpunity_asset3d_cat', // the taxonomy
+//      array(
+//          'description'=> 'A Consumer is a game object that consumes energy (e.g. a building).',
+//          'slug' => 'consumer',
+//      )
+//  );
+//  $inserted_term8 = get_term_by('slug', 'consumer', 'wpunity_asset3d_cat');
+//  update_term_meta($inserted_term8->term_id, 'wpunity_yamlmeta_assetcat_pat', wpunity_default_value_consumer_get(), true);
+//  update_term_meta($inserted_term8->term_id, 'wpunity_assetcat_gamecat', 2 , true);
 
-  wp_insert_term(
-      'Producer', // the term
-      'wpunity_asset3d_cat', // the taxonomy
-      array(
-          'description'=> 'A Producer is a game object that generates energy (e.g. a Wind Turbine or a Solar Panel).',
-          'slug' => 'producer',
-      )
-  );
-  $inserted_term9 = get_term_by('slug', 'producer', 'wpunity_asset3d_cat');
-  update_term_meta($inserted_term9->term_id, 'wpunity_yamlmeta_assetcat_pat', wpunity_default_value_producer_get(), true);
-  update_term_meta($inserted_term9->term_id, 'wpunity_assetcat_gamecat', 2 , true);
+//  wp_insert_term(
+//      'Producer', // the term
+//      'wpunity_asset3d_cat', // the taxonomy
+//      array(
+//          'description'=> 'A Producer is a game object that generates energy (e.g. a Wind Turbine or a Solar Panel).',
+//          'slug' => 'producer',
+//      )
+//  );
+//  $inserted_term9 = get_term_by('slug', 'producer', 'wpunity_asset3d_cat');
+//  update_term_meta($inserted_term9->term_id, 'wpunity_yamlmeta_assetcat_pat', wpunity_default_value_producer_get(), true);
+//  update_term_meta($inserted_term9->term_id, 'wpunity_assetcat_gamecat', 2 , true);
 
   wp_insert_term(
       'Marker', // the term
